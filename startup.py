50e12594b1a56643338a49955c7b1c1a0e53d52c
Xara25 <azzouni2007abd@gmail.com>
2023-12-02 08:05:22 +0300

    Changing ui importing method

--- startup.py
+++ startup.py
@@ -1,48 +1,12 @@
 import sys
-from PySide6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QPlainTextEdit,
-    QWidget,
-    QVBoxLayout,
-    QPushButton,
-)
-from PySide6.QtGui import QFont
-from PySide6.QtCore import Qt
-
-app = QApplication([])
+from PySide6.QtWidgets import QApplication, QWidget
+from startup_ui import Ui_Form
 
 
-class Window(QWidget):
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle("Flash Text")
-        self.resize(750, 512)
-        self.center()
-
-        self.label = QLabel("Enter Text", self)
-        self.label.setFont(QFont("Arial", 30))
-        self.input = QPlainTextEdit(self)
-        self.button = QPushButton("Send", self)
-        self.button.setFixedSize(125, 50)
-        self.button.setFont(QFont("Arial", 20))
-        self.button.clicked.connect(self.Clicked)
-
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(
-            self.label,
-            alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter,
-        )
-        self.vbox.addWidget(self.input)
-        self.vbox.addWidget(
-            self.button,
-            alignment=Qt.AlignmentFlag.AlignRight,
-        )
-        self.setLayout(self.vbox)
+        self.setupUi(self)
 
     def center(self):
         qr = self.frameGeometry()
@@ -51,10 +15,12 @@ class Window(QWidget):
         self.move(qr.topLeft())
 
     def Clicked(self):
-        print(self.input.toPlainText())
+        with open("data.txt", "w") as f:
+            f.write(self.input.toPlainText())
 
 
 if __name__ == "__main__":
+    app = QApplication([])
     window = Window()
     window.show()
     sys.exit(app.exec())
